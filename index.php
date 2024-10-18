c7a702459f33a48a45b522f154b3375c7a971e8f
Nagaya Makoto <makoto@longroof.jp>
2024-10-18 11:46:51 +0900

    [modify]index.phpへの追加

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
 echo "Hello Azure Nagaya241018 World!";
+echo "Hello Azure Nagaya241018 World!";
+echo "Hello Azure Nagaya241018 World!";
+echo "Hello Azure Nagaya241018 World!";
+echo "Hello Azure Nagaya241018 World!";
+echo "Hello Azure Nagaya241018 World!";
+echo "Hello Azure Nagaya241018 World!";
 
 ?>
